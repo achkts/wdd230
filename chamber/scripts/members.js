040502725b7fcf0afca168957e50e368a89c1dfb
achkts <thi01003@byui.edu>
2023-11-11 23:23:45 -0700

    fixedphone number in json

--- chamber/scripts/members.js
+++ chamber/scripts/members.js
@@ -11,6 +11,7 @@ async function getLinks(linksURL) {
 
     console.log(data);
     displayLinks(data.members);
+    displayList(data.members);
 }; 
 
 getLinks(linksURL);
@@ -21,15 +22,6 @@ const displayLinks = (members) => {
     members.forEach((member) => {
         let card = document.createElement("div");
         card.setAttribute('class', 'memberCard');
-
-        // "name": "Tacomania",
-        //     "address": "3673 W 13400 S, #G, Riverton, Utah 84065",
-        //     "phone number": "801-989-7912",
-        //     "website": "https://www.facebook.com/TacomaniaUT/",
-        //     "imgUrl": "https://scontent-sjc3-1.xx.fbcdn.net/v/t39.30808-6/294686868_469550678507501_1123540739016470752_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=5f2048&_nc_ohc=6jhifHf4sSwAX9rsGmf&_nc_ht=scontent-sjc3-1.xx&oh=00_AfCSEQ8_0L4HC40eDLnGFowUuIzOy0j7TZbAmQXDa9vfdg&oe=6553196F",
-        //     "membership level": "Silver",
-        //     "date joined": "2022"
-        
 
         let imgUrl = document.createElement("img");
         imgUrl.setAttribute('src', member.imgUrl);
@@ -64,6 +56,44 @@ const displayLinks = (members) => {
 
         console.log(members);
         cards.appendChild(card);
+    })
+     
+
+}
+const displayList = (members) => {
+    members.forEach((member) => {
+        let card = document.createElement("div");
+        card.setAttribute('class', 'memberLine');
+
+        let name = document.createElement("div");
+        name.textContent = member.name;
+        card.appendChild(name);
+
+        let address = document.createElement("div");
+        address.innerHTML = member.address;
+        card.appendChild(address);
+
+        let phoneNum = document.createElement("div");
+        phoneNum.textContent = member.phoneNum;
+        card.appendChild(phoneNum);
+
+        let website = document.createElement("div");
+        website.textContent = member.website;
+        card.appendChild(website);
+
+       
+
+        let membershipLevel = document.createElement("div");
+        membershipLevel.textContent = member.membershipLevel;
+        card.appendChild(membershipLevel);
+
+        let dateJoined = document.createElement("div");
+        dateJoined.textContent = member.dateJoined;
+        card.appendChild(dateJoined);
+      
+
+        console.log(members);
+        lists.appendChild(card);
     })
      
 
