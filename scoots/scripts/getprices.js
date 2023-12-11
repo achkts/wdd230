97bddcaf49e3a5109dedc712b83fec82f5c63cd8
achkts <thi01003@byui.edu>
2023-12-11 12:27:31 -0700

    rental table

--- scoots/scripts/getprices.js
+++ scoots/scripts/getprices.js
@@ -1,3 +1,55 @@
-const baseURL = "https://achkts.github.io/wdd230/scoots";
+const baseURL = "https://achkts.github.io/wdd230/scoots/scootsIndex.html";
 
-const linksURL = "https://achkts.github.io/wdd230/chamber/data/members.json";
+const linksURL = "https://achkts.github.io/wdd230/scoots/data/prices.json";
+
+const prices = document.querySelector('#rentalType');
+
+async function getLinks(linksURL) {
+    let response = await fetch(linksURL);
+    let data = await response.json();
+
+    console.log(data);
+    displayLinks(data.prices);
+    displayList(data.prices);
+}; 
+
+getLinks(linksURL);
+
+const displayLinks = (prices) => {
+    prices.forEach((price) => {
+        let table = document.createElement("tr");
+        table.setAttribute('class', 'rentalPricing');
+
+        let rentalType = document.createElement("td");
+        rentalType.textContent = price.rentalType;
+        table.appendChild(rentalType);
+
+        let maxPerson = document.createElement("td");
+        maxPerson.textContent = price.maxPerson;
+        table.appendChild(maxPerson);
+
+        let reservationHalf = document.createElement("td");
+        reservationHalf.textContent = price.reservationHalf;
+        table.appendChild(reservationHalf);
+
+        let reservationFull = document.createElement("td");
+        reservationFull.textContent = price.reservationFull;
+        table.appendChild(reservationFull);
+
+        let walkHalf = document.createElement("td");
+        walkHalf.textContent = price.walkHalf;
+        table.appendChild(walkHalf);
+
+        let walkFull = document.createElement("td");
+        walkFull.textContent = price.walkFull;
+        table.appendChild(walkFull);
+
+        
+      
+
+        console.log(prices);
+        prices.appendChild(price);
+    })
+     
+
+}
