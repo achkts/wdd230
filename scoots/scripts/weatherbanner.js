445a05590a45d7872396a386bcd19d7a00788220
achkts <thi01003@byui.edu>
2023-12-13 16:25:00 -0700

    css changes mobile

---
+++ scoots/scripts/weatherbanner.js
@@ -0,0 +1,11 @@
+const date = new Date();
+const banner = document.getElementById('weatherBanner');
+
+if (today.getDay() != 1 && today.getDay() != 2 && today.getDay() != 3) {
+    banner.classList.add('hideBanner');
+}
+
+const bannerClose = document.getElementById('bannerClose');
+bannerClose.addEventListener('click', () => {
+    banner.classList.add('hideBanner');
+})
